b09f0e465c0212d4656903dbf47a40757e11e935
AndreaG <andreag19892@gmail.com>
2026-02-23 20:26:11 +0100

    aggiunto calcolo del bilancio da backend in modo tale che con una sola chiamata API si sa già il bilancio tra entrate e uscite

--- server/src/controllers/transaction.controller.js
+++ server/src/controllers/transaction.controller.js
@@ -1,4 +1,5 @@
 const Transaction = require ( '../models/Transaction.model' );
+const mongoose = require ( 'mongoose' );
 
 exports.addTransaction = async ( req, res ) => {
     try {
@@ -33,6 +34,43 @@ exports.getTransaction = async ( req, res ) => {
             success: true,
             count: transazioni.length,
             data: transazioni
+        } );
+    } catch (error) {
+        res.status ( 500 ).json ( {
+            success: false,
+            message: error.message
+        } )
+    }
+}
+
+exports.getBalance = async ( req, res ) => {
+    try {
+        const {userId} = req.params;
+        const stats = await Transaction.aggregate ( [
+            // 1. filtra solo le transazioni dell'utente specifico
+            {$match: {userId: new mongoose.Types.ObjectId ( userId )},},
+            //2. raggruppa e somma in base al tipo (in, out)
+            {
+                $group: {
+                    _id: "$type",
+                    total: {$sum: "$amount"}
+                }
+            }
+        ] );
+        //formattiamo i dati per renderli facili da leggere al frontend
+        const balance = {
+            entrate: 0,
+            uscite: 0,
+            totale: 0
+        };
+        stats.forEach ( item => {
+            if ( item._id === 'IN' ) balance.entrate = item.total;
+            if ( item._id === 'OUT' ) balance.uscite = item.total;
+        } );
+        balance.totale = balance.entrate - balance.uscite;
+        res.status ( 200 ).json ( {
+            success: true,
+            data: balance
         } );
     } catch (error) {
         res.status ( 500 ).json ( {
--- server/src/routes/transaction.routes.js
+++ server/src/routes/transaction.routes.js
@@ -1,8 +1,9 @@
 const express = require ( 'express' );
 const router = express.Router ();
-const {addTransaction, getTransaction} = require ( '../controllers/transaction.controller' );
+const {addTransaction, getTransaction, getBalance} = require ( '../controllers/transaction.controller' );
 
 router.post ( '/add', addTransaction );
 router.get ( '/:userId', getTransaction );
+router.get ( '/balance/:userId', getBalance );
 
 module.exports = router;
