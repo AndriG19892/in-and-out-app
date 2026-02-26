e0305f39496c31c0860170de31e5e09e13b453e9
AndreaG <andreag19892@gmail.com>
2026-02-26 19:02:13 +0100

    creato un interceptor di axios in modo tale da non passare l'userId direttamente nella chiamata

---
+++ client/src/api/axiosConfig.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+//creo un istanza personalizzata
+const api = axios.create ( {
+    baseURL: 'http://localhost:5000/api',
+} );
+
+//Aggiungiamo un INTERCEPTOR
+api.interceptors.request.use ( config => {
+        const token = localStorage.getItem ( 'token' );
+        if ( token ) {
+            //se il token c'è lo aggiungiamo all'header Authorization
+            config.headers.Authorization = `Bearer ${ token }`;
+        }
+        return config;
+    },
+    ( error ) => {
+        return Promise.reject ( error )
+    }
+);
+
+//Interceptor per le risposte:
+api.interceptors.response.use (
+    ( response ) => response,
+    ( error ) => {
+        if ( error.response && error.response.status === 401 ) {
+            //se il server risponde 401, il token è scaduto o invalido
+            localStorage.clear ();
+            window.location.href = '/login';
+        }
+        return Promise.reject ( error );
+    }
+);
+
+export default api;
