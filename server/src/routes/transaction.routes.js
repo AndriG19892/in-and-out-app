b5f4c45f6cd8327ecc8cc2f096723e690395b8a8
AndriG19892 <68709776+AndriG19892@users.noreply.github.com>
2026-02-27 11:17:04 +0100

    Add deleteTransaction route to transaction.routes.js

--- server/src/routes/transaction.routes.js
+++ server/src/routes/transaction.routes.js
@@ -1,9 +1,10 @@
 const express = require ( 'express' );
 const router = express.Router ();
-const {addTransaction, getTransaction, getBalance} = require ( '../controllers/transaction.controller' );
+const {addTransaction, getTransaction, getBalance,deleteTransaction } = require ( '../controllers/transaction.controller' );
 const {protect} = require ( '../middleware/auth.middleware' );
 router.post ( '/add', protect, addTransaction );
 router.get ( '/:userId', protect, getTransaction );
 router.get ( '/balance/:userId', protect, getBalance );
+router.delete('/:id', protect, transactionController.deleteTransaction);
 
 module.exports = router;
