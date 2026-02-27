a235ad93dc0ea6e04424cc1190d0a479e8a14272
AndriG19892 <68709776+AndriG19892@users.noreply.github.com>
2026-02-27 08:41:56 +0100

    Fix variable name case in axiosConfig.js

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -7,7 +7,7 @@ const api = axios.create ( {
     baseURL: baseURL,
 } );
 
-console.log("url backend",baseUrl);
+console.log("url backend",baseURL);
 
 //Aggiungiamo un INTERCEPTOR
 api.interceptors.request.use ( config => {
